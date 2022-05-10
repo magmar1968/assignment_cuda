484161bf969f350014ac4b1465781e9c18811412
lorenzo magnoni <lorenzo.magnoni@studenti.unimi.it>
2022-05-10 14:07:42 +0200

    alcune modifiche secondarie

--- es2_RandomNumber/src/myRandom.h
+++ es2_RandomNumber/src/myRandom.h
@@ -5,6 +5,10 @@
 
 namespace rnd
 {
+
+
+  // =========================
+  //      abstract class 
     template<typename T>
     class MyRandom
     {
@@ -23,39 +27,67 @@ namespace rnd
         }
     };
 
-    template< typename T>
-    class LinGenCongruential : 
-        public MyRandom<T>
-    {
-      public:
-        /**
-         * @brief Construct a new Linnear Generator Congruential object that generate random
-         *        number following the rule: S_{i+1} = (a* S_i + b) mod m
-         * 
-         * @param seed 
-         * @param a (default 1664525)
-         * @param b (default 1013904223)
-         * @param m (default MAX_INT) 
-         */
-        LinGenCongruential(int seed, uint a = 1664525, uint b = 1013904223, uint m = INT32_MAX)
-            :_current(seed),_a(a),_b(b),_m(m)
-            {};
-        ~LinGenCongruential(){};
+  // =========================
+  //   linear congruential
 
-        T genUniform(const T min = 0, const T max = 1) 
-        {
-            _current = ( _a * _current + _b) % _m;
-            T num = _current / (T) _m;
-            return num * max + min;
-        };
+  template< typename T>
+  class LinGenCongruential : 
+    public MyRandom<T>
+  {
+    public:
+      /**
+       * @brief Construct a new Linnear Generator Congruential object that generate random
+       *        number following the rule: S_{i+1} = (a* S_i + b) mod m
+       * 
+       * @param seed 
+       * @param a (default 1664525)
+       * @param b (default 1013904223)
+       * @param m (default MAX_INT) 
+       */
+      LinGenCongruential(int seed, uint a = 1664525, uint b = 1013904223, uint m = UINT_MAX)
+        :_current(seed),_a(a),_b(b),_m(m)
+      {};
+      ~LinGenCongruential(){};
+      T genUniform(const T min = 0, const T max = 1) 
+      {
+          _current = ( _a * _current + _b) % _m;
+          T num = _current / (T) _m;
+          return num * max + min;
+    }
+    private:
+      uint _a, _b, _m; 
+      uint _current;
+  };
+
+  template<typename T>
+  class GenTausworth :
+    public MyRandom<T>
+  {
+    public:
+      GenTausworth(uint seed = 256, uint k1 = 13, uint k2 = 19,uint k3 = 12, uint m = 4294967294UL)
+        :_current(seed),_k1(k1),_k2(k2),_k3(k3),_m(m)
+      {
+          if(seed < 128)
+          {
+              _current += 128;
+              std::cerr<< "ERROR: in __FUNCTION__             \n"
+                       << "       seed must be grater than 128\n";
+          } 
+      }
+
+      T genUniform(const T min = 0, const T max = 1)
+      {
+          uint b    = (((_current << _k1) ^ _current ) >> _k2);
+          _current  = (((_current & _m ) << _k3) ^ b);
+          T num = _current / (T)_m;
+          return num * max + min;
         
+      }
 
-      private:
-        uint _a, _b, _m; 
-        uint _current;
-    };
-
-
+    private: 
+      uint _k1,_k2,_k3,_m;
+      uint _current;
+  };
 
 
 
