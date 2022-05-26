52e72e95a42ebc0669952ace114edd48c2b4dcf4
Matteo Martelli <matteo.martelli1@studenti.unimi.itgit config --global user.name Matteo>
2022-05-26 18:38:15 +0200

    step I

---
+++ es3_DeviceHost/src/stoch.cu
@@ -0,0 +1,12 @@
+#include "stoch.h"
+
+StochProcess::StochProcess(double mu, double sigma)
+{
+    _mu = mu;
+    _sigma = sigma;
+}
+
+double StochProcess::EulerDoStep(double p, double dt, double random)
+{
+    return p * (_mu * dt + _sigma * random * sqrt(dt));
+}
---
+++ es3_DeviceHost/src/stoch.h
@@ -0,0 +1,20 @@
+#ifndef __STOCH__
+#define __STOCH__
+
+#include <cmath>
+
+class StochProcess
+{
+    public:
+    StochProcess(double mu, double sigma);
+    double EulerDoStep(double p, double dt, double random);
+    double ExactDoStep(double dt);
+
+    private:
+    double _mu;
+    double _sigma;
+
+};
+
+
+#endif
