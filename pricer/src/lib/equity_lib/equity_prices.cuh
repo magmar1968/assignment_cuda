62b30be30419c9df76ddb4ea2a4c98dd2d45891d
lorenzo magnoni <lorenzo.magnoni@studenti.unimi.it>
2022-06-13 09:18:29 +0200

    brief and HD flag added

--- pricer/src/lib/equity_lib/equity_prices.cuh
+++ pricer/src/lib/equity_lib/equity_prices.cuh
@@ -1,64 +1,79 @@
+/**
+ * @file equity_prices.cuh
+ * @author Lorenzo Magnoni - Andrea Ripamonti
+ * @brief  Tiene traccia del prezzo di un determinato numero (_dim) di sottostanti
+ *         al tempo _time. I sottostanti interessati sono contenuti nel vettore 
+ *         _eq_descr. La classe può lavorare sia su device che host
+ * 
+ */
 #ifndef __EQUITY_PRICES__
 #define __EQUITY_PRICES__
 
 #include "equity_description.cuh"
 
+#define H  __host__
+#define D  __device__
+#define HD __host__ __device__
+
+
 class Equity_prices
 {
   private:
-    double   _time;
-    double * _prices;
-    size_t   _dim;
+    double   _time;      //current time
+    double * _prices;    //current price
+    size_t   _dim;       //# equities
     Equity_description ** _eq_descr;
   
   public:
-    Equity_prices(){}
-    Equity_prices(double time,
+    //constructors & destructors
+    HD Equity_prices(){}
+    HD Equity_prices(double time,
                   double * prices,
                   size_t   dim,
                   Equity_description ** equity_descriptions)
         :_time(time),_prices(prices),_dim(dim),
         _eq_descr(equity_descriptions)
     {}
+    HD ~Equity_prices(){}
 
     // getter & setter
-    double Get_time()const{ return _time;}
-    double Get_eq_price(const size_t i) const
+    HD double Get_time()const{ return _time;}
+    HD double Get_eq_price(const size_t i) const
     {
         if(i < _dim)
             return _prices[i];
         else
             exit(1); //forse non funzica su cuda
     }
-    double Get_eq_price() const {return Get_eq_price(0);}
-    double Get_dim() const {return _dim;}
+    HD double Get_eq_price() const {return Get_eq_price(0);}
+    HD double Get_dim() const {return _dim;}
 
-    Equity_description * Get_eq_description(const size_t i) const
+    HD Equity_description * Get_eq_description(const size_t i) const
     {
         if(i < _dim)
             return _eq_descr[i];
         else
             exit(1); //forse non funzica su cuda
     }
-    Equity_description * Get_eq_description() const {return Get_eq_description(0);}
+    HD Equity_description * Get_eq_description() const {return Get_eq_description(0);}
     
+    //setters
+    HD void Set_time(double t) {_time = t;}
 
-    void Set_time(double t) {_time = t;}
-
-    void Set_eq_price(size_t i, double price)
+    HD void Set_eq_price(size_t i, double price)
     {
         if ( i < _dim)
             _prices[i] = price;
         else
             exit(1); //forse non funzica su cuda
     }
-    void Set_eq_price(double price){ Set_eq_price(0,price);}
+    HD void Set_eq_price(double price){ Set_eq_price(0,price);}
 
-    void Set_eq_description(size_t i , Equity_description * eq_description)
+    HD void Set_eq_description(size_t i , Equity_description * eq_description)
     {
         _eq_descr[i] = eq_description; 
     }
-    void Set_eq_description(Equity_description * eq_description)
+    HD void Set_eq_description(Equity_description * eq_description)
     {
         Set_eq_description(0,eq_description);
     }
