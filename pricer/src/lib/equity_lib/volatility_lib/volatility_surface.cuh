d3d61e92d3744fd43b9626545b022ed7edea52b5
lorenzo magnoni <lorenzo.magnoni@studenti.unimi.it>
2022-06-13 09:22:40 +0200

    brief and HD added

--- pricer/src/lib/equity_lib/volatility_lib/volatility_surface.cuh
+++ pricer/src/lib/equity_lib/volatility_lib/volatility_surface.cuh
@@ -1,5 +1,17 @@
+/**
+ * @file volatility_surface.cuh
+ * @author Lorenzo Magnoni - Andrea Ripamonti
+ * @brief Descrive una generica superficie di volatilità. Al momento è disponibile
+ *        solo il caso banale di una curva piatta. La classe gira sia su host che 
+ *        device.
+ */
 #ifndef __VOLATILITY__
 #define __VOLATILITY__
+
+#define H  __host__
+#define D  __device__
+#define HD __host__ __device__
+
 
 class Volatility_surface
 {
@@ -8,12 +20,12 @@ class Volatility_surface
   
   public:
     //constructor & destructor
-    Volatility_surface() {}
-    Volatility_surface( double vol)
+    HD Volatility_surface() {}
+    HD Volatility_surface( double vol)
         :_vol(vol)
     {}
     // getter
-    double Get_volatility(double t_start, double t_end) const
+    HD double Get_volatility(double t_start, double t_end) const
     {
         return _vol;
     }
