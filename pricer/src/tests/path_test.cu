2cf2385cc200ca7565ebcee309df7a99301a3b5e
Andre-Ripa-99 <andre.ripa@outlook.it>
2022-06-22 17:12:35 +0200

    Update path_test.cu

--- pricer/src/tests/path_test.cu
+++ pricer/src/tests/path_test.cu
@@ -14,32 +14,30 @@
 #include "../lib/support_lib/parse_lib/parse_lib.cuh"
 
 
-#define NPATH 5  //number of paths
+#define NPATH 1  //number of paths             //1 solo cammino
 #define STEPS 5  // number of steps
 #define NEQ 5  //number of equities
 
 
-__global__ void kernel(double*);
-D void createPath_device(Process_eq*, Equity_prices*,  Schedule*, Path*, size_t, double*);
-H void createPath_host(Process_eq*, Equity_prices*, Schedule*, Path*, size_t);
-HD void createPath_generic(Process_eq*, Equity_prices*, Schedule*, Path* , size_t);
+__global__ void kernel(double*, uint* );
+D void createPath_device(Equity_prices*, Schedule*, double*, size_t, uint*);
+H void createPath_host(Equity_prices*, Schedule*, double*, size_t, uint*);
+HD void createPath_generic(Process_eq*, Equity_prices*, Schedule*, double* , size_t);
 
 
 
 
-__global__ void kernel(double* path_out)
+__global__ void kernel(double* path_out, uint* seeds)
 {
-    rnd::GenCombined* gnr_in = new rnd::GenCombined(800, 200, 400, 500);
-    Process_eq_lognormal_multivariante* process_in = new Process_eq_lognormal_multivariante(gnr_in, NEQ);
-    pricer::udb start_prices[NEQ];
+    pricer::udb start_prices[NEQ];                              //definiamo oggetti dentro a kernel
     for (int i = 0; i < NEQ; i++)
     {
-        start_prices[i] = 100 * (1 + i) + i;
+        start_prices[i] = 100 * i;
     }
     double start_time = 0.15;
     Equity_description** descr = new Equity_description * [NEQ];
 
-    Volatility_surface* vol = new Volatility_surface(0.01);
+    Volatility_surface* vol = new Volatility_surface(0.5);
     Yield_curve_flat* yc = new Yield_curve_flat("euro", 0);
 
     for (int i = 0; i < NEQ; i++)
@@ -61,44 +59,58 @@ __global__ void kernel(double* path_out)
     }
     Schedule* calen = new Schedule(tempi, STEPS);
 
-    Path* cammini_GPU = new Path[NPATH];
-
-    createPath_device(process_in, starting_point_in, calen, cammini_GPU, NPATH, path_out);
-}
-
-D void createPath_device(Process_eq* process,
-    Equity_prices* starting_point,
-    Schedule* calendar,
-    Path* cammini,
-    size_t totpaths,
-    double* path_out)
-{
-    size_t index = blockIdx.x * blockDim.x + threadIdx.x;
-    while (index < totpaths)
+    for (size_t i = 0; i < NEQ; i++)
     {
-        createPath_generic(process, starting_point, calendar, cammini, index);
-	
-	for (size_t j = 0; j < NEQ; j++)
-        {
-            //path_out[i] 0 = new double[STEPS];
-            for (size_t k = 0; k < STEPS; k++)
-            {
-                path_out[index] = cammini[index].Get_equity_prices(0)->Get_eq_price(0).get_number();
-            }
-        }
-        index += blockDim.x * gridDim.x;
+        path_out[i] = 0;
     }
+    createPath_device(starting_point_in, calen, path_out, NPATH, seeds);
+   
 }
 
-H void createPath_host(Process_eq* process,
-    Equity_prices* starting_point,
+D void createPath_device(Equity_prices* starting_point,
     Schedule* calendar,
-    Path* cammini,
-    size_t totpaths)
+    double* path_out,
+    size_t totpaths,
+    uint* seeds)
 {
+    size_t index = blockIdx.x * blockDim.x + threadIdx.x;     //in questo test è sempre 1 (vedere sotto: kernel <<<1,1>>>)
+    uint seed0;
+    uint seed1;
+    uint seed2;
+    uint seed3;
+                                          //blocco valido in generale, anche se index non è 1
+    seed0 = seeds[0 + index*4];
+    seed1 = seeds[1 + index*4];
+    seed2 = seeds[2 + index*4];
+    seed3 = seeds[3 + index*4];
+
+    rnd::GenCombined* gnr_in = new rnd::GenCombined(seed0, seed1, seed2, seed3);
+    Process_eq_lognormal_multivariante* process = new Process_eq_lognormal_multivariante(gnr_in, NEQ);
+
+    createPath_generic(process, starting_point, calendar, path_out, index);
+      
+}
+
+H void createPath_host(Equity_prices* starting_point,
+    Schedule* calendar,
+    double* path_out,
+    size_t totpaths,
+    uint* seeds)
+{
+    uint seed0;
+    uint seed1;
+    uint seed2;
+    uint seed3;
     for (size_t index = 0; index < totpaths; index++)
     {
-        createPath_generic(process, starting_point, calendar, cammini, index);
+        seed0 = seeds[0 + index*4];
+        seed1 = seeds[1 + index*4];
+        seed2 = seeds[2 + index*4];
+        seed3 = seeds[3 + index*4];
+        rnd::GenCombined* gnr_in = new rnd::GenCombined(seed0, seed1, seed2, seed3);
+        Process_eq_lognormal_multivariante* process = new Process_eq_lognormal_multivariante(gnr_in, NEQ);
+
+        createPath_generic(process, starting_point, calendar, path_out, index);
     }
 }
 
@@ -106,21 +118,30 @@ H void createPath_host(Process_eq* process,
 HD void createPath_generic(Process_eq* process,
     Equity_prices* starting_point,
     Schedule* calendar,
-    Path* cammini,
+    double* path_out ,
     size_t index)
 {
-        cammini[index] = Path(starting_point, calendar, process);
+        Path cammino = Path(starting_point, calendar, process);
+        for (size_t i = 0; i < NEQ; i++)
+        {
+            path_out[i] = cammino.Get_equity_prices(STEPS - 1)->Get_eq_price(i).get_number(); //path out contiente i prezzi delle equities allo step finale
+        }
 }
+        
 
 
 int main(int argc, char **argv)
 {
-
-    srand(time(NULL));
     cudaError_t cudaStatus;
-
+    srand(time(NULL));
     size_t* npath = new size_t(NPATH);
     size_t* neq = new size_t(NEQ);
+
+    uint* seeds = new uint[4 * NPATH];
+    for (size_t inc = 0; inc < 4 * NPATH; inc++)
+    {
+        seeds[inc] = rnd::genSeed(true);       //same seeds for gpu and cpu --> same results? let's check!
+    }
 
     prcr::Device dev;
     dev.CPU = false;
@@ -128,23 +149,21 @@ int main(int argc, char **argv)
 
     if (prcr::cmdOptionExists(argv, argv + argc, "-gpu"))
         dev.GPU = true;
-    if (prcr::cmdOptionExists(argv, argv + argc, "-cpu"))
+    //if (prcr::cmdOptionExists(argv, argv + argc, "-cpu"))
         dev.CPU = true;
 
     if (dev.CPU == true)
     {
-
-        rnd::GenCombined* gnr_in = new rnd::GenCombined(800, 200, 400, 500);
-        Process_eq_lognormal_multivariante* process_in = new Process_eq_lognormal_multivariante(gnr_in, NEQ);
+                                                        //assegnazione dei campi negli oggetti
         pricer::udb start_prices[NEQ];
         for (int i = 0; i < NEQ; i++)
         {
-            start_prices[i] = 100 * (1 + i) + i;
+            start_prices[i] = 100 * i;
         }
         double start_time = 0.15;
         Equity_description** descr = new Equity_description * [NEQ];
 
-        Volatility_surface* vol = new Volatility_surface(0.01);
+        Volatility_surface* vol = new Volatility_surface(0.5);
         Yield_curve_flat* yc = new Yield_curve_flat("euro", 0);
 
         for (int i = 0; i < NEQ; i++)
@@ -159,6 +178,7 @@ int main(int argc, char **argv)
         }
 
         Equity_prices* starting_point_in = new Equity_prices(start_time, start_prices, NEQ, descr);
+
         double tempi[STEPS];
         for (size_t k = 0; k < STEPS; k++)
         {
@@ -166,124 +186,72 @@ int main(int argc, char **argv)
         }
         Schedule* calen = new Schedule(tempi, STEPS);
 
+        double* path_CPU = new double[NEQ]; //contiene i prezzi delle equities allo step finale
 
-
-
-        Path* cammini_CPU = new Path[NPATH];
-        createPath_host(process_in, starting_point_in, calen, cammini_CPU, NPATH);
+        createPath_host(starting_point_in, calen, path_CPU, NPATH, seeds);
 
 
         //stampa CPU
-
-        std::cout << std::endl << "paths:" << std::endl;
-        for (int i = 0; i < NPATH; i++)
+        std::cout << std::endl << "results:\n\n" << std::endl;
+        for (int k = 0; k < NEQ; k++)
         {
-            std::cout << "\n\n\n\npath " << i << ":" << std::endl;
+            std::cout << "\nequity " << k << ":";
+            std::cout << "\t" << path_CPU[k] << "\n";
+        }
+        std::cout << std::endl;
+
+        if (dev.GPU == true)           //se -gpu setted
+        {
+
+            //CudaSetDevice(0);
+            double* dev_path;
+            double* path_GPU = new double[NEQ];
+
+            uint* dev_seeds;
+
+            cudaStatus = cudaMalloc((void**)&dev_path, NEQ * sizeof(double));             //malloc vettore che contiene prezzo equities allo step finale
+            if (cudaStatus != cudaSuccess) { fprintf(stderr, "cudaMalloc1 failed!\n"); }
+
+            cudaStatus = cudaMalloc((void**)&dev_seeds, NPATH * 4 * sizeof(uint));       //malloc dei seeds
+            if (cudaStatus != cudaSuccess) { fprintf(stderr, "cudaMalloc2 failed!\n"); }
+
+            cudaStatus = cudaMemcpy(dev_seeds, seeds, NPATH * 4 * sizeof(uint), cudaMemcpyHostToDevice);   //memcpy seeds su device
+            if (cudaStatus != cudaSuccess) { fprintf(stderr, "cudaMemcpy1 failed!\n"); }
+            fprintf(stderr, "%s\n", cudaGetErrorString(cudaStatus));
+
+            kernel << <1, 1 >> > (dev_path, dev_seeds);
+            cudaStatus = cudaGetLastError();
+            if (cudaStatus != cudaSuccess) { fprintf(stderr, "Kernel failed: %s\n", cudaGetErrorString(cudaStatus)); }
+
+            cudaStatus = cudaMemcpy(path_GPU, dev_path, NEQ * sizeof(double), cudaMemcpyDeviceToHost);  //memcpy path_out da device
+            if (cudaStatus != cudaSuccess) { fprintf(stderr, "cudaMemcpy2 failed!\n"); }
+            fprintf(stderr, "%s\n", cudaGetErrorString(cudaStatus));
+
+
+            //stampa  GPU
             for (int k = 0; k < NEQ; k++)
             {
-                std::cout << "\nequity " << k << ":" << std::endl;
-                for (int j = 0; j < STEPS; j++)
-                    std::cout << cammini_CPU[i].Get_equity_prices(j)->Get_eq_price(k).get_number() << " ";
+                std::cout << "\nequity " << k << ":";
+                std::cout << "\t" << path_GPU[k] << "\n";
             }
-            std::cout << std::endl;
-            std::cout << std::endl;
-        }
-    }
 
 
 
 
 
 
-
-
-
-
-    if (dev.GPU == true)
-    {
-        //CudaSetDevice(0);
-        double* dev_paths;
-        double* paths = new double[NPATH];
-	/*for (size_t t = 0; t < NPATH; t++)
-	{
-		paths[t] = new double*[STEPS];
-		for(size_t y = 0; y < STEPS; y++)
-		{
-			paths[t][y] = new double[NEQ];
-		}
-	}*/ 	
-
-        cudaStatus = cudaMalloc((void**)&dev_paths, NPATH*sizeof(double));
-        if (cudaStatus != cudaSuccess) { fprintf(stderr, "cudaMalloc failed!\n"); }
-
-        kernel << <32, 32 >> > (dev_paths);
-        cudaStatus = cudaGetLastError();
-        if (cudaStatus != cudaSuccess) { fprintf(stderr, "Kernel failed: %s\n", cudaGetErrorString(cudaStatus)); }
-
-        cudaStatus = cudaMemcpy(paths,dev_paths, NPATH * sizeof(double), cudaMemcpyDeviceToHost);
-        if (cudaStatus != cudaSuccess) { fprintf(stderr, "cudaMemcpy failed!\n"); }
-	fprintf(stderr, "%s\n", cudaGetErrorString(cudaStatus));
-        //stampa  GPU
-
-        std::cout << std::endl << "paths:" << std::endl;
-        for (int i = 0; i < NPATH; i++)
-        {
-            std::cout << "\n\n\n\npath " << i << ":" << std::endl;
+            //confronto con cpu
+            int err_msg = 0;
+	        double minimum;
             for (int k = 0; k < NEQ; k++)
             {
-                std::cout << "\nequity " << k << ":" << std::endl;
-                for (int j = 0; j < STEPS; j++)
-                    std::cout << paths[i];
-            }
-            std::cout << std::endl;
-            std::cout << std::endl;
+		    minimum = min(path_CPU[k], path_GPU[k]);  //minimo tra prezzo della stessa equity al final step tra gpu e cpu
+            if (abs(path_CPU[k] - path_GPU[k])>0.000001*minimum) 
+                err_msg += pow(2, k + 1);  //se i valori si discostano troppo tra di loro ("troppo definito da quale tra i due è min)
+            }                              //ritorniamo errore (così costruito, è unico per ogni caso)
+            return err_msg;
         }
-
     }
-    
-
-
+    std::cout << std::endl;
     return 0;
-
 }
-
-
-/*cudaStatus = cudaMalloc((void**)&dev_process, sizeof(Process_eq));
-if (cudaStatus != cudaSuccess) { fprintf(stderr, "cudaMalloc 1 failed!\n"); }
-
-cudaStatus = cudaMalloc((void**)&dev_prices, sizeof(Equity_prices));
-if (cudaStatus != cudaSuccess) { fprintf(stderr, "cudaMalloc 2 failed!\n"); }
-
-cudaStatus = cudaMalloc((void**)&dev_schedule, sizeof(Schedule));
-if (cudaStatus != cudaSuccess) { fprintf(stderr, "cudaMalloc 3 failed!\n"); }
-
-cudaStatus = cudaMalloc((void**)&dev_totpaths, sizeof(size_t));
-if (cudaStatus != cudaSuccess) { fprintf(stderr, "cudaMalloc 4 failed!\n"); }
-
-cudaStatus = cudaMalloc((void**)&dev_paths, NPATH * sizeof(Path));
-if (cudaStatus != cudaSuccess) { fprintf(stderr, "cudaMalloc 6 failed!\n"); }
-
-
-
-
-cudaStatus = cudaMemcpy(dev_process, process_in, sizeof(Process_eq), cudaMemcpyHostToDevice);
-if (cudaStatus != cudaSuccess) { fprintf(stderr, "cudaMemcpy 1 failed!\n"); }
-
-cudaStatus = cudaMemcpy(dev_prices, starting_point_in, sizeof(Equity_prices), cudaMemcpyHostToDevice);
-if (cudaStatus != cudaSuccess) { fprintf(stderr, "cudaMemcpy 2 failed!\n"); }
-
-cudaStatus = cudaMemcpy(dev_schedule, calen, sizeof(Schedule), cudaMemcpyHostToDevice);
-if (cudaStatus != cudaSuccess) { fprintf(stderr, "cudaMemcpy 3 failed!\n"); }
-
-cudaStatus = cudaMemcpy(dev_totpaths, npath, sizeof(size_t), cudaMemcpyHostToDevice);
-if (cudaStatus != cudaSuccess) { fprintf(stderr, "cudaMemcpy 4 failed!\n"); }
-
-
-
-kernel << <32, 32 >> > (process_in, starting_point_in, calen, cammini_GPU, NPATH);
-cudaStatus = cudaGetLastError();
-if (cudaStatus != cudaSuccess) { fprintf(stderr, "Kernel failed: %s\n", cudaGetErrorString(cudaStatus)); }
-
-
-cudaStatus = cudaMemcpy(cammini_GPU, dev_paths, NPATH * sizeof(Path), cudaMemcpyDeviceToHost);
-if (cudaStatus != cudaSuccess) { fprintf(stderr, "cudaMemcpy backward failed!\n"); }*/
