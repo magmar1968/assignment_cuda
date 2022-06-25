3519173542064a46aff38ff0ab8d5dad1c17b611
Andre-Ripa-99 <andre.ripa@outlook.it>
2022-06-25 16:15:08 +0200

    First montecarlo class test
    Funziona

--- pricer/src/lib/contract_option_lib/contract_eq_option_vanilla/contract_eq_option_vanilla.cu
+++ pricer/src/lib/contract_option_lib/contract_eq_option_vanilla/contract_eq_option_vanilla.cu
@@ -4,15 +4,16 @@
 __host__ __device__ double
 Contract_eq_option_vanilla::Pay_off(const Path *path)
 {
-    size_t laste_step = path -> Get_dim();
-    Equity_prices * final_price = path -> Get_equity_prices(laste_step);
+    size_t last_step = path -> Get_dim();
+    Equity_prices * final_price = path -> Get_equity_prices(last_step-1);
 
     size_t n_eq = final_price -> Get_dim();
     double S_f = 0;
 
     for(size_t i = 0; i < n_eq; ++i)
     {
-        S_f += final_price -> Get_eq_price(i).get_number();
+        S_f += final_price->Get_eq_price(i).get_number();
+        
     }
     return Pay_off_vanilla(S_f);
 }
--- pricer/src/tests/montecarlo_test1.cu
+++ pricer/src/tests/montecarlo_test1.cu
@@ -20,11 +20,11 @@
 #include "../../../lib/support_lib/myDouble_lib/myudouble.cuh"
 
 
-#define STEPS 10  // number of steps
+#define STEPS 5  // number of steps
 #define NEQ 1  //number of equities
-#define NBLOCKS 32  //cuda blocks
-#define TPB 32    //threads per block
-#define PPT 10    //paths per thread
+#define NBLOCKS 10  //cuda blocks
+#define TPB 200    //threads per block
+#define PPT 100    //paths per thread
 
 
 
@@ -48,9 +48,9 @@ struct Schedule_arguments
 struct Eq_description_arguments
 {
     double vol[STEPS];
-    std::string isin_code[12];
-    std::string name[30];
-    std::string currency[20];
+    char isin_code[12];
+    char name[30];
+    char currency[20];
     double div_yield;
     double yc[STEPS];
 };
@@ -66,9 +66,9 @@ struct Eq_prices_arguments
 
 
 __global__ void kernel_mc(uint*, Schedule_arguments*, Eq_description_arguments*, Eq_prices_arguments*, Vanilla_arguments*, Result*);
-D void simulate_device(uint*, Contract_option*, Result*);
-H void simulate_host(uint*, Contract_option*, Result*);
-HD void simulate_generic(uint*, uint, Contract_option*, Result*);
+D void simulate_device(uint*, Contract_eq_option_vanilla*, Result*);
+H void simulate_host(uint*, Contract_eq_option_vanilla*, Result*);
+HD void simulate_generic(uint*, int, Contract_eq_option_vanilla*, Result*);
 
 
 
@@ -77,7 +77,7 @@ __global__ void kernel_mc(uint* dev_seeds,
     Schedule_arguments* dev_sched,
     Eq_description_arguments* dev_descr,
     Eq_prices_arguments* dev_prices,
-    Vanilla_arguments dev_vnl_args,
+    Vanilla_arguments* dev_vnl_args,
     Result* dev_results)
 {
     pricer::udb start_prices[NEQ];                              //definiamo oggetti dentro a kernel
@@ -89,15 +89,15 @@ __global__ void kernel_mc(uint* dev_seeds,
 
     double start_time = dev_prices->start_time;
     Volatility_surface* vol = new Volatility_surface(dev_descr->vol[0]);
-    char* currency = dev_descr->currency->c_str();
-    Yield_curve_term_structure* yc = new Yield_curve_term_structure(dev_descr->currency->c_str(), dev_descr->yc[0], dev_sched->tempi[0], STEPS);
+    char* currency =(dev_descr->currency);
+    Yield_curve_term_structure* yc = new Yield_curve_term_structure(currency, dev_descr->yc, dev_sched->tempi, STEPS);
 
     for (int i = 0; i < NEQ; i++)
     {
         descr[i] = new Equity_description;
         descr[i]->Set_isin_code(dev_descr->isin_code);
-        descr[i]->Set_name(dev_descr->name->c_str());
-        descr[i]->Set_currency(dev_descr->currency->c_str());
+        descr[i]->Set_name(dev_descr->name);
+        descr[i]->Set_currency(dev_descr->currency);
         descr[i]->Set_dividend_yield(dev_descr->div_yield);
         descr[i]->Set_yc(yc);
         descr[i]->Set_vol_surface(vol);
@@ -106,16 +106,16 @@ __global__ void kernel_mc(uint* dev_seeds,
     Equity_prices* starting_point_in = new Equity_prices(start_time, start_prices, NEQ, descr);
     Schedule* calen = new Schedule(dev_sched->tempi, NEQ);
 
-    Contract_option* contr_opt;
+    Contract_eq_option_vanilla* contr_opt;
     contr_opt = new Contract_eq_option_vanilla(starting_point_in, calen,
-        dev_vnl_args.strike_price, dev_vnl_args.contract_type);
+        dev_vnl_args->strike_price, dev_vnl_args->contract_type);
 
     simulate_device(dev_seeds, contr_opt, dev_results);
 
 }
 
 D void simulate_device(uint* seeds,
-    Contract_option* contr_opt,
+    Contract_eq_option_vanilla* contr_opt,
     Result* results)
 {
     size_t index = blockIdx.x * blockDim.x + threadIdx.x;
@@ -123,10 +123,10 @@ D void simulate_device(uint* seeds,
 }
 
 H void simulate_host(uint* seeds,
-    Contract_option* contr_opt,
+    Contract_eq_option_vanilla* contr_opt,
     Result* results)
 {
-    for (size_t index = 0; index < NBLOCKS * TPB * PPT; index++)   //l'espressione che risulta dalla moltiplicazione è NPATH
+    for (size_t index = 0; index < NBLOCKS*TPB; index++)   //l'espressione che risulta dalla moltiplicazione è il numero totale di threads
     {
         simulate_generic(seeds, index, contr_opt, results);
     }
@@ -135,7 +135,7 @@ H void simulate_host(uint* seeds,
 
 HD void simulate_generic(uint* seeds,
     int index,
-    Contract_option* contr_opt,
+    Contract_eq_option_vanilla* contr_opt,
     Result* results)
 {
     uint seed0 = seeds[0 + index * 4];
@@ -158,9 +158,9 @@ int main(int argc, char** argv)
 {
     cudaError_t cudaStatus;
     uint NPATH = NBLOCKS * TPB * PPT;
-    srand(time(NULL));
+    //srand(time(NULL));
 
-
+    
     Result host_results[NBLOCKS * TPB];
 
     uint* seeds = new uint[4 * NPATH];
@@ -169,51 +169,54 @@ int main(int argc, char** argv)
         seeds[inc] = rnd::genSeed(true);
     }
 
-
+    
     Vanilla_arguments vnl_args;
 
-    vnl_args.contract_type = char("P");
+    vnl_args.contract_type = 'C';
     vnl_args.strike_price = 105;
 
 
-
-    Schedule_arguments sch_args;
+    
+    Schedule_arguments* sch_args = new Schedule_arguments;
 
     double dt = 0.3;
+    //sch_args->tempi = new double[STEPS];
     for (size_t k = 0; k < STEPS; k++)
     {
-        sch_args.tempi[k] = k * dt;
+        sch_args->tempi[k] = (k+1) * dt;
+    }
+
+    
+    Eq_description_arguments* dscrp_args = new Eq_description_arguments;
+
+    for (size_t i = 0; i < STEPS; i++)
+    {
+        dscrp_args->vol[i] = 0.5;
+    }
+    strcpy(dscrp_args->isin_code, "qwertyuiopas");
+    strcpy(dscrp_args->name, "opzione di prova");
+    strcpy(dscrp_args->currency, "£");
+    dscrp_args->div_yield = 0;
+    for (size_t k = 0; k < STEPS; k++)
+    {
+        dscrp_args->yc[k] = sqrt(k);
     }
 
 
-    Eq_description_arguments dscrp_args;
-
-
-    dscrp_args.vol = 0.25;
-    dscrp_args.isin_code = "qwertyuiopas";
-    dscrp_args.name = "opzione di prova";
-    dscrp_args.currency = "£";
-    dscrp_args.div_yield = 0;
-    for (size_t k = 0; k < STEPS; k++)
-    {
-        dscrp_args.yc[k] = sqrt(k);
-    }
-
-
-
+    
     Eq_prices_arguments prices_args;
 
     prices_args.start_time = 0;
     for (size_t k = 0; k < NEQ; k++)
     {
-        prices_args.start_prices[k] = k * 100;
+        prices_args.start_prices[k] = (k+1) * 100;
     }
 
 
 
 
     prcr::Device dev;
-    dev.CPU = false;
+    dev.CPU = true;
     dev.GPU = false;
 
     if (prcr::cmdOptionExists(argv, argv + argc, "-gpu"))
@@ -226,25 +229,26 @@ int main(int argc, char** argv)
     {
         Equity_description** descr = new Equity_description * [NEQ];
 
-        Volatility_surface* vol = new Volatility_surface(dscrp_args.vol[0]);
-        Yield_curve_term_structure* yc = new Yield_curve_term_structure(dscrp_args.currency, dscrp_args.yc, sch_args.tempi, STEPS);
+        Volatility_surface* vol = new Volatility_surface(dscrp_args->vol[0]);
+        char* currency = (dscrp_args->currency);
+        Yield_curve_term_structure* yc = new Yield_curve_term_structure(currency, dscrp_args->yc, sch_args->tempi, STEPS);
 
         for (int i = 0; i < NEQ; i++)
         {
             descr[i] = new Equity_description;
-            descr[i]->Set_isin_code(dscrp_args.isin_code);
-            descr[i]->Set_name(dscrp_args.name);
-            descr[i]->Set_currency(dscrp_args.currency);
-            descr[i]->Set_dividend_yield(dscrp_args.div_yield);
+            descr[i]->Set_isin_code(dscrp_args->isin_code);
+            descr[i]->Set_name(dscrp_args->name);
+            descr[i]->Set_currency(dscrp_args->currency);
+            descr[i]->Set_dividend_yield(dscrp_args->div_yield);
             descr[i]->Set_yc(yc);
-            descr[i]->Set_vol_surface(&vol);
+            descr[i]->Set_vol_surface(vol);
         }
 
         Equity_prices* starting_point_in = new Equity_prices(prices_args.start_time, &(prices_args.start_prices[0]), NEQ, descr);
 
-        Schedule* calen = new Schedule(sch_args.tempi, STEPS);
+        Schedule* calen = new Schedule(sch_args->tempi, STEPS);
 
-        Contract_option* contr_opt;
+        Contract_eq_option_vanilla* contr_opt;
         contr_opt = new Contract_eq_option_vanilla(starting_point_in, calen,
             vnl_args.strike_price, vnl_args.contract_type);
 
@@ -306,7 +310,7 @@ int main(int argc, char** argv)
 
 
 
-        kernel_mc << < 32, 32 >> > (dev_seeds, dev_sched, dev_descr, dev_prices, dev_vnl_args, dev_results);
+        kernel_mc <<< 32,32 >>> (dev_seeds, dev_sched, dev_descr, dev_prices, dev_vnl_args, dev_results);
         cudaStatus = cudaGetLastError();
         if (cudaStatus != cudaSuccess) { fprintf(stderr, "Kernel failed: %s\n", cudaGetErrorString(cudaStatus)); }
 
@@ -339,7 +343,9 @@ int main(int argc, char** argv)
     final_res.opt_price /= (NBLOCKS * TPB);
 
     std::cout << final_res.opt_price;
-    return 0;
+    if (final_res.opt_price - 1900.77 <0.000001)
+        return 0;
+    else
+        return 1;
 
 }
-
