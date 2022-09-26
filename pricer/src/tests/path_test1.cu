12bb7b73a8297a286953f50fc104b4a83605a405
lorenzo magnoni <lorenzo.magnoni@studenti.unimi.it>
2022-09-26 15:09:31 +0200

    extreme path_test1

--- pricer/src/tests/path_test1.cu
+++ pricer/src/tests/path_test1.cu
@@ -151,225 +151,228 @@ HD void simulate_generic(uint* seeds,
 
 int main(int argc, char** argv)
 {
-    cudaError_t cudaStatus;
-    srand(time(NULL));
-
-    int* host_cuda_bool = new int(0);
-    //*host_cuda_bool = true;
-
-    double* host_results = new double[NBLOCKS * TPB];
-    for (int i = 0; i < NBLOCKS * TPB; i++)
+    for(int i = 0; i < 1000; ++i)
     {
-        host_results[i] = 0;
-    }
+        cudaError_t cudaStatus;
+        srand(time(NULL));
 
-    uint* seeds = new uint[4 * NBLOCKS * TPB];
-    for (size_t inc = 0; inc < 4 * NBLOCKS * TPB; inc++)
-    {
-        seeds[inc] = rnd::genSeed(true);
-    }
+        int* host_cuda_bool = new int(0);
+        //*host_cuda_bool = true;
 
-    Vanilla_data* vnl_args = new Vanilla_data;
+        double* host_results = new double[NBLOCKS * TPB];
+        for (int i = 0; i < NBLOCKS * TPB; i++)
+        {
+            host_results[i] = 0;
+        }
 
-    vnl_args->contract_type = 'C';
-    vnl_args->strike_price = 100;
+        uint* seeds = new uint[4 * NBLOCKS * TPB];
+        for (size_t inc = 0; inc < 4 * NBLOCKS * TPB; inc++)
+        {
+            seeds[inc] = rnd::genSeed(true);
+        }
 
-    Schedule_data* sch_args = new Schedule_data;
+        Vanilla_data* vnl_args = new Vanilla_data;
 
-    double dt = 0.3;
-    for (size_t k = 0; k < STEPS; k++)
-    {
-        sch_args->tempi[k] = (k + 1) * dt;
-    }
+        vnl_args->contract_type = 'C';
+        vnl_args->strike_price = 100;
 
-    Eq_description_data* dscrp_args = new Eq_description_data;
+        Schedule_data* sch_args = new Schedule_data;
 
-    for (size_t i = 0; i < STEPS; i++)
-    {
-        dscrp_args->vol[i] = 0.0;
-    }
-    strcpy(dscrp_args->isin_code, "codice isin");
-    strcpy(dscrp_args->name, "opzione di prova");
-    strcpy(dscrp_args->currency, "euro");
-    dscrp_args->div_yield = 0;
-    for (size_t k = 0; k < STEPS; k++)
-    {
-        dscrp_args->yc[k] = 0.0;
-    }
+        double dt = 0.3;
+        for (size_t k = 0; k < STEPS; k++)
+        {
+            sch_args->tempi[k] = (k + 1) * dt;
+        }
 
+        Eq_description_data* dscrp_args = new Eq_description_data;
 
-
-    Eq_prices_data* prices_args = new Eq_prices_data;
-
-    prices_args->start_time = 0;
-    prices_args->start_prices = 125.;
-
-
-    Device dev;
-    //dev.CPU = false;
-    //dev.GPU = false;
-
-    if (prcr::cmdOptionExists(argv, argv + argc, "-gpu"))
-        dev.GPU = true;
-    if (prcr::cmdOptionExists(argv, argv + argc, "-cpu"))
-        dev.CPU = true;
-
-
-    if (dev.CPU == true)
-    {
-        Equity_description* descr = new Equity_description;
-
-            descr->Set_isin_code(dscrp_args->isin_code);
-            descr->Set_name(dscrp_args->name);
-        char* currency = (dscrp_args->currency);
-            descr->Set_currency(dscrp_args->currency);
-        Yield_curve_term_structure* yc = new Yield_curve_term_structure(currency, dscrp_args->yc, sch_args->tempi, STEPS);
-            descr->Set_dividend_yield(dscrp_args->div_yield);
-            descr->Set_yc(yc);
-        Volatility_surface* vol = new Volatility_surface(dscrp_args->vol[0]);
-            descr->Set_vol_surface(vol);
-
-        Equity_prices* starting_point_in = new Equity_prices(prices_args->start_time, prices_args->start_prices, descr);
-        Schedule* calen = new Schedule(sch_args->tempi, STEPS);
-        Contract_eq_option_vanilla* contr_opt;
-            contr_opt = new Contract_eq_option_vanilla(starting_point_in, calen,
-                vnl_args->strike_price, vnl_args->contract_type);
-
-
-        simulate_host(seeds, contr_opt, host_results, host_cuda_bool);
-
-        delete(descr);
-        delete(yc);
-        delete(vol);
-        delete(starting_point_in);
-        delete(calen);
-        delete(contr_opt);
-
-    }
+        for (size_t i = 0; i < STEPS; i++)
+        {
+            dscrp_args->vol[i] = 0.0;
+        }
+        strcpy(dscrp_args->isin_code, "codice isin");
+        strcpy(dscrp_args->name, "opzione di prova");
+        strcpy(dscrp_args->currency, "euro");
+        dscrp_args->div_yield = 0;
+        for (size_t k = 0; k < STEPS; k++)
+        {
+            dscrp_args->yc[k] = 0.0;
+        }
 
 
 
+        Eq_prices_data* prices_args = new Eq_prices_data;
 
-    if (dev.GPU == true)
-    {
-
-        //CudaSetDevice(0);
-        uint* dev_seeds;
-        Schedule_data* dev_sched;
-        Eq_description_data* dev_descr;
-        Eq_prices_data* dev_prices;
-        Vanilla_data* dev_vnl_args;
-        double* dev_res = new double[NBLOCKS * TPB];
-        int* dev_cuda_bool = new int(0);
+        prices_args->start_time = 0;
+        prices_args->start_prices = 125.;
 
 
+        Device dev;
+        //dev.CPU = false;
+        //dev.GPU = false;
 
-        cudaStatus = cudaMalloc((void**)&dev_seeds, NBLOCKS * TPB * 4 * sizeof(uint));
-        if (cudaStatus != cudaSuccess) { fprintf(stderr, "cudaMalloc1 failed!\n"); }
-
-        cudaStatus = cudaMalloc((void**)&dev_sched, sizeof(Schedule_data));
-        if (cudaStatus != cudaSuccess) { fprintf(stderr, "cudaMalloc2 failed!\n"); }
-
-        cudaStatus = cudaMalloc((void**)&dev_descr, sizeof(Eq_description_data));
-        if (cudaStatus != cudaSuccess) { fprintf(stderr, "cudaMalloc3 failed!\n"); }
-
-        cudaStatus = cudaMalloc((void**)&dev_prices, sizeof(Eq_prices_data));
-        if (cudaStatus != cudaSuccess) { fprintf(stderr, "cudaMalloc4 failed!\n"); }
-
-        cudaStatus = cudaMalloc((void**)&dev_res, NBLOCKS * TPB * sizeof(double));
-        if (cudaStatus != cudaSuccess) { fprintf(stderr, "cudaMalloc5 failed!\n"); }
-
-        cudaStatus = cudaMalloc((void**)&dev_vnl_args, NBLOCKS * TPB * sizeof(Vanilla_data));
-        if (cudaStatus != cudaSuccess) { fprintf(stderr, "cudaMalloc6 failed!\n"); }
-       
-        cudaStatus = cudaMalloc((void**)&dev_cuda_bool, sizeof(int));
-        if (cudaStatus != cudaSuccess) { fprintf(stderr, "cudaMalloc7 failed!\n"); }
-
-        cudaStatus = cudaMemcpy(dev_cuda_bool, host_cuda_bool, sizeof(int), cudaMemcpyHostToDevice);
-        if (cudaStatus != cudaSuccess) { fprintf(stderr, "cudaMemcpy0 failed! %s\n", cudaGetErrorString(cudaStatus)); }
-
-        cudaStatus = cudaMemcpy(dev_seeds, seeds, NBLOCKS * TPB * 4 * sizeof(uint), cudaMemcpyHostToDevice);
-        if (cudaStatus != cudaSuccess) { fprintf(stderr, "cudaMemcpy1 failed!\n"); }
-        fprintf(stderr, "%s\n", cudaGetErrorString(cudaStatus));
-
-        cudaStatus = cudaMemcpy(dev_sched, sch_args, sizeof(Schedule_data), cudaMemcpyHostToDevice);
-        if (cudaStatus != cudaSuccess) { fprintf(stderr, "cudaMemcpy2 failed!\n"); }
-        fprintf(stderr, "%s\n", cudaGetErrorString(cudaStatus));
-
-        cudaStatus = cudaMemcpy(dev_descr, dscrp_args, sizeof(Eq_description_data), cudaMemcpyHostToDevice);
-        if (cudaStatus != cudaSuccess) { fprintf(stderr, "cudaMemcpy3 failed!\n"); }
-        fprintf(stderr, "%s\n", cudaGetErrorString(cudaStatus));
-
-        cudaStatus = cudaMemcpy(dev_prices, prices_args, sizeof(Eq_prices_data), cudaMemcpyHostToDevice);
-        if (cudaStatus != cudaSuccess) { fprintf(stderr, "cudaMemcpy4 failed!\n"); }
-        fprintf(stderr, "%s\n", cudaGetErrorString(cudaStatus));
-
-        cudaStatus = cudaMemcpy(dev_vnl_args, vnl_args, sizeof(Vanilla_data), cudaMemcpyHostToDevice);
-        if (cudaStatus != cudaSuccess) { fprintf(stderr, "cudaMemcpy5 failed!\n"); }
-        fprintf(stderr, "%s\n", cudaGetErrorString(cudaStatus));
-
-        cudaStatus = cudaMemcpy(dev_res,host_results, NBLOCKS*TPB*sizeof(double), cudaMemcpyHostToDevice);
-        if (cudaStatus != cudaSuccess) { fprintf(stderr, "cudaMemcpy6 failed!\n"); }
-        fprintf(stderr, "%s\n", cudaGetErrorString(cudaStatus));
-
-        kernel_mc << < NBLOCKS, TPB >> > (dev_seeds, dev_sched, dev_descr, dev_prices, dev_vnl_args, dev_res, dev_cuda_bool);
-        cudaStatus = cudaGetLastError();
-        if (cudaStatus != cudaSuccess) { fprintf(stderr, "Kernel failed: %s\n", cudaGetErrorString(cudaStatus)); }
-
-        cudaFree(dev_seeds);
-        cudaFree(dev_sched);
-        cudaFree(dev_descr);
-        cudaFree(dev_prices);
-        cudaFree(dev_vnl_args);
+        if (prcr::cmdOptionExists(argv, argv + argc, "-gpu"))
+            dev.GPU = true;
+        if (prcr::cmdOptionExists(argv, argv + argc, "-cpu"))
+            dev.CPU = true;
 
 
-        cudaStatus = cudaMemcpy(host_results, dev_res, NBLOCKS * TPB * sizeof(double), cudaMemcpyDeviceToHost);
-        if (cudaStatus != cudaSuccess) { fprintf(stderr, "cudaMemcpy backwards1 failed!\n"); }
-        printf("%d", cudaStatus);
-        fprintf(stderr, "%s\n", cudaGetErrorString(cudaStatus));
+        if (dev.CPU == true)
+        {
+            Equity_description* descr = new Equity_description;
 
-        cudaStatus = cudaMemcpy(host_cuda_bool, dev_cuda_bool, sizeof(int), cudaMemcpyDeviceToHost);
-        if (cudaStatus != cudaSuccess) { fprintf(stderr, "cudaMemcpy back2 failed!\n"); }
+                descr->Set_isin_code(dscrp_args->isin_code);
+                descr->Set_name(dscrp_args->name);
+            char* currency = (dscrp_args->currency);
+                descr->Set_currency(dscrp_args->currency);
+            Yield_curve_term_structure* yc = new Yield_curve_term_structure(currency, dscrp_args->yc, sch_args->tempi, STEPS);
+                descr->Set_dividend_yield(dscrp_args->div_yield);
+                descr->Set_yc(yc);
+            Volatility_surface* vol = new Volatility_surface(dscrp_args->vol[0]);
+                descr->Set_vol_surface(vol);
 
+            Equity_prices* starting_point_in = new Equity_prices(prices_args->start_time, prices_args->start_prices, descr);
+            Schedule* calen = new Schedule(sch_args->tempi, STEPS);
+            Contract_eq_option_vanilla* contr_opt;
+                contr_opt = new Contract_eq_option_vanilla(starting_point_in, calen,
+                    vnl_args->strike_price, vnl_args->contract_type);
+
+
+            simulate_host(seeds, contr_opt, host_results, host_cuda_bool);
+
+            delete(descr);
+            delete(yc);
+            delete(vol);
+            delete(starting_point_in);
+            delete(calen);
+            delete(contr_opt);
+
+        }
+
+
+
+
+        if (dev.GPU == true)
+        {
+
+            //CudaSetDevice(0);
+            uint* dev_seeds;
+            Schedule_data* dev_sched;
+            Eq_description_data* dev_descr;
+            Eq_prices_data* dev_prices;
+            Vanilla_data* dev_vnl_args;
+            double* dev_res = new double[NBLOCKS * TPB];
+            int* dev_cuda_bool = new int(0);
+
+
+
+            cudaStatus = cudaMalloc((void**)&dev_seeds, NBLOCKS * TPB * 4 * sizeof(uint));
+            if (cudaStatus != cudaSuccess) { fprintf(stderr, "cudaMalloc1 failed!\n"); }
+
+            cudaStatus = cudaMalloc((void**)&dev_sched, sizeof(Schedule_data));
+            if (cudaStatus != cudaSuccess) { fprintf(stderr, "cudaMalloc2 failed!\n"); }
+
+            cudaStatus = cudaMalloc((void**)&dev_descr, sizeof(Eq_description_data));
+            if (cudaStatus != cudaSuccess) { fprintf(stderr, "cudaMalloc3 failed!\n"); }
+
+            cudaStatus = cudaMalloc((void**)&dev_prices, sizeof(Eq_prices_data));
+            if (cudaStatus != cudaSuccess) { fprintf(stderr, "cudaMalloc4 failed!\n"); }
+
+            cudaStatus = cudaMalloc((void**)&dev_res, NBLOCKS * TPB * sizeof(double));
+            if (cudaStatus != cudaSuccess) { fprintf(stderr, "cudaMalloc5 failed!\n"); }
+
+            cudaStatus = cudaMalloc((void**)&dev_vnl_args, NBLOCKS * TPB * sizeof(Vanilla_data));
+            if (cudaStatus != cudaSuccess) { fprintf(stderr, "cudaMalloc6 failed!\n"); }
+        
+            cudaStatus = cudaMalloc((void**)&dev_cuda_bool, sizeof(int));
+            if (cudaStatus != cudaSuccess) { fprintf(stderr, "cudaMalloc7 failed!\n"); }
+
+            cudaStatus = cudaMemcpy(dev_cuda_bool, host_cuda_bool, sizeof(int), cudaMemcpyHostToDevice);
+            if (cudaStatus != cudaSuccess) { fprintf(stderr, "cudaMemcpy0 failed! %s\n", cudaGetErrorString(cudaStatus)); }
+
+            cudaStatus = cudaMemcpy(dev_seeds, seeds, NBLOCKS * TPB * 4 * sizeof(uint), cudaMemcpyHostToDevice);
+            if (cudaStatus != cudaSuccess) { fprintf(stderr, "cudaMemcpy1 failed!\n"); }
+            fprintf(stderr, "%s\n", cudaGetErrorString(cudaStatus));
+
+            cudaStatus = cudaMemcpy(dev_sched, sch_args, sizeof(Schedule_data), cudaMemcpyHostToDevice);
+            if (cudaStatus != cudaSuccess) { fprintf(stderr, "cudaMemcpy2 failed!\n"); }
+            fprintf(stderr, "%s\n", cudaGetErrorString(cudaStatus));
+
+            cudaStatus = cudaMemcpy(dev_descr, dscrp_args, sizeof(Eq_description_data), cudaMemcpyHostToDevice);
+            if (cudaStatus != cudaSuccess) { fprintf(stderr, "cudaMemcpy3 failed!\n"); }
+            fprintf(stderr, "%s\n", cudaGetErrorString(cudaStatus));
+
+            cudaStatus = cudaMemcpy(dev_prices, prices_args, sizeof(Eq_prices_data), cudaMemcpyHostToDevice);
+            if (cudaStatus != cudaSuccess) { fprintf(stderr, "cudaMemcpy4 failed!\n"); }
+            fprintf(stderr, "%s\n", cudaGetErrorString(cudaStatus));
+
+            cudaStatus = cudaMemcpy(dev_vnl_args, vnl_args, sizeof(Vanilla_data), cudaMemcpyHostToDevice);
+            if (cudaStatus != cudaSuccess) { fprintf(stderr, "cudaMemcpy5 failed!\n"); }
+            fprintf(stderr, "%s\n", cudaGetErrorString(cudaStatus));
+
+            cudaStatus = cudaMemcpy(dev_res,host_results, NBLOCKS*TPB*sizeof(double), cudaMemcpyHostToDevice);
+            if (cudaStatus != cudaSuccess) { fprintf(stderr, "cudaMemcpy6 failed!\n"); }
+            fprintf(stderr, "%s\n", cudaGetErrorString(cudaStatus));
+
+            kernel_mc << < NBLOCKS, TPB >> > (dev_seeds, dev_sched, dev_descr, dev_prices, dev_vnl_args, dev_res, dev_cuda_bool);
+            cudaStatus = cudaGetLastError();
+            if (cudaStatus != cudaSuccess) { fprintf(stderr, "Kernel failed: %s\n", cudaGetErrorString(cudaStatus)); }
+
+            cudaFree(dev_seeds);
+            cudaFree(dev_sched);
+            cudaFree(dev_descr);
+            cudaFree(dev_prices);
+            cudaFree(dev_vnl_args);
+
+
+            cudaStatus = cudaMemcpy(host_results, dev_res, NBLOCKS * TPB * sizeof(double), cudaMemcpyDeviceToHost);
+            if (cudaStatus != cudaSuccess) { fprintf(stderr, "cudaMemcpy backwards1 failed!\n"); }
+            printf("%d", cudaStatus);
+            fprintf(stderr, "%s\n", cudaGetErrorString(cudaStatus));
+
+            cudaStatus = cudaMemcpy(host_cuda_bool, dev_cuda_bool, sizeof(int), cudaMemcpyDeviceToHost);
+            if (cudaStatus != cudaSuccess) { fprintf(stderr, "cudaMemcpy back2 failed!\n"); }
+
+
+            
+
+            cudaFree(dev_res);
+            cudaFree(dev_cuda_bool);
+
+            
+
+
+
+        }
+
+        /*if (!*host_cuda_bool)
+        {
+            printf("Something went wrong... \n"); //codice di errore intero?
+        }*/
+
+        //statistica finale --- scrivere funzione media per struct?
+        double final_res;
+        for (size_t i = 0; i < NBLOCKS * TPB; i++)
+        {
+            final_res += host_results[i];
+        }
+        final_res /= double(NBLOCKS * TPB);
+
+        std::cout << "\n" << final_res << std::endl;
+        //std::cout << final_res.error << std::endl;
 
         
 
-        cudaFree(dev_res);
-        cudaFree(dev_cuda_bool);
-
-        
-
-
+        //all delete
+        delete(host_cuda_bool);
+        delete(host_results);
+        delete(vnl_args);
+        delete(sch_args);
+        delete(dscrp_args);
+        delete(prices_args);
+        delete[](seeds);
 
     }
-
-    /*if (!*host_cuda_bool)
-    {
-        printf("Something went wrong... \n"); //codice di errore intero?
-    }*/
-
-    //statistica finale --- scrivere funzione media per struct?
-    double final_res;
-    for (size_t i = 0; i < NBLOCKS * TPB; i++)
-    {
-        final_res += host_results[i];
-    }
-    final_res /= double(NBLOCKS * TPB);
-
-    std::cout << "\n" << final_res << std::endl;
-    //std::cout << final_res.error << std::endl;
-
-    
-
-    //all delete
-    delete(host_cuda_bool);
-    delete(host_results);
-    delete(vnl_args);
-    delete(sch_args);
-    delete(dscrp_args);
-    delete(prices_args);
-    delete[](seeds);
-
 
     return 0;
 
