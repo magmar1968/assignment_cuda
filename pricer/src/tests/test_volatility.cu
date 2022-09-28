5b714d585f9ae2e4cd0e60bccb43d7cd5a1779a9
lorenzo magnoni <lorenzo.magnoni@studenti.unimi.it>
2022-09-28 09:25:09 +0200

    test concluded

--- pricer/src/tests/test_volatility.cu
+++ pricer/src/tests/test_volatility.cu
@@ -2,13 +2,13 @@
 
 
 
-__host__             bool run_device( prcr::Pricer_args *,prcr::Vol_args *);
+__host__             bool  run_device( prcr::Pricer_args *,prcr::Vol_args *);
 
 __global__           void kernel(prcr::Pricer_args *,prcr::Vol_args *);
 
-__host__             bool simulate_host(prcr::Pricer_args *,prcr::Vol_args *);
+__host__             bool  simulate_host(prcr::Pricer_args *,prcr::Vol_args *);
 __device__           void simulate_device(prcr::Pricer_args *, prcr::Volatility_surface *,prcr::Vol_args *);
-__host__ __device__  void simulate_generic();
+__host__ __device__  void simulate_generic(size_t index, prcr::Volatility_surface *,prcr::Vol_args *);
 
 
 __host__ bool
@@ -44,12 +44,16 @@ run_device(prcr::Pricer_args * prcr_args, prcr::Vol_args * host_vol_args)
     if (cudaStatus != cudaSuccess) { fprintf(stderr, "cudaMemcpy3 failed!\n"); }
     fprintf(stderr, "%s\n", cudaGetErrorString(cudaStatus));
 
-
+    bool kernel_error_check = true;
+    for (int i = 0; i < NBLOCKS*TPB; i++)
+    {
+	    kernel_error_check = kernel_error_check && (host_vol_args[i].vol == prcr_args->vol_args.vol);  //controlla che simulate_generic abbia 										  //agito correttamente
+    }
 
     cudaFree(dev_prcr_args);
     cudaFree(dev_vol_args);
 
-    return cudaStatus;
+    return ((cudaStatus==cudaSuccess) && (kernel_error_check));
 }
 
 __global__ void 
@@ -61,6 +65,47 @@ kernel(prcr::Pricer_args * prcr_args, prcr::Vol_args * vol_args)
     simulate_device(prcr_args,vol_srfc,vol_args);
 
     delete(vol_srfc);
+}
+
+__device__ void
+simulate_device(prcr::Pricer_args        * prcr_args, 
+                prcr::Volatility_surface * vol_sfrc,
+                prcr::Vol_args           * vol_args)
+{
+    size_t index   = blockIdx.x * blockDim.x + threadIdx.x;
+    size_t NBLOCKS = prcr_args->dev_opts.N_blocks;
+    size_t TPB     = prcr_args->dev_opts.N_threads;
+    if (index < NBLOCKS * TPB) simulate_generic( index, vol_sfrc,vol_args);
+}
+
+__host__ bool
+simulate_host(prcr::Pricer_args * prcr_args,
+              prcr::Vol_args    * vol_args)
+{
+    using namespace prcr;
+    size_t NBLOCKS = prcr_args->dev_opts.N_blocks;
+    size_t TPB = prcr_args->dev_opts.N_threads;
+    Volatility_surface * vol_srfc = new Volatility_surface(prcr_args->vol_args.vol);
+
+
+    for (int index = 0; index < NBLOCKS * TPB; index++){
+        simulate_generic(index,vol_srfc,vol_args);
+    }
+    bool stat = true;
+    for (int j = 0; j < NBLOCKS * TPB; j++)
+    {
+        stat = stat && (vol_args[j].vol == prcr_args->vol_args.vol); //controllo che simulate generic abbia sovrascritto correttamente il campo
+    }
+    return stat;
+}
+
+
+__device__ __host__ void
+simulate_generic(size_t index,
+                 prcr::Volatility_surface * vol_sfrc,
+                 prcr::Vol_args *           vol_args)
+{
+    vol_args[index].vol = vol_sfrc->Get_volatility();
 }
 
 int main(int argc, char ** argv)
@@ -85,11 +130,11 @@ int main(int argc, char ** argv)
     }
 
     if(GPU == true)
-        for(int i = 0; i < 10000; ++i)
+        for(int i = 0; i < N_TEST_SIM; ++i)
             status = status && run_device(prcr_args,host_vol_args);
     
     if(CPU == true)
-        for(int i = 0; i < 10000; ++i)
+        for(int i = 0; i < N_TEST_SIM; ++i)
             status = status && simulate_host(prcr_args,host_vol_args);
     
         
