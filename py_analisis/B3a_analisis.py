a63033e9df66b770d16eaff8dd3bb32c2898ec7f
lorenzo magnoni <lorenzo.magnoni@studenti.unimi.it>
2022-10-20 13:31:36 +0200

    update

--- py_analisis/B3a_analisis.py
+++ py_analisis/B3a_analisis.py
@@ -5,13 +5,14 @@ import matplotlib.pyplot as plt
 df = pd.read_csv("../pricer/data/outfile_B3a_m_values.csv")
 
 m_values = df["m"].tolist()
-exact_vals = df["p_off_exact"].to_list()
-approx_vals = df["p_off_approx"].to_list()
+exact_vals = df["exact_result"].to_list()
+approx_vals = df["approx_result"].to_list()
 
 
 plt.plot(m_values,approx_vals,label = r"pay off approx")
 plt.plot(m_values,exact_vals, label = r"pay off exact ")
+plt.yscale('log')
 
 
 plt.legend()
-plt.savefig("../plots/B3a_plot",format="png")
+plt.savefig("../plots/B3a_plot.png",format="png")
